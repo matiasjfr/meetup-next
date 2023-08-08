e60bb602ae5b4568c7a1d657b753926dde8d9e7a
Matías Fitó <matias.fito.reussi@modo.com.ar>
2023-08-08 08:17:49 -0300

    fix: ci 14

--- next.config.js
+++ next.config.js
@@ -3,12 +3,6 @@ const nextConfig = {
     images: {
         domains: ['loremflickr.com', 'picsum.photos'],
       },
-      output: 'export',
-      distDir: '_static',
-      images: {
-        unoptimized: true
-      }
-    
 }
 
 module.exports = nextConfig
