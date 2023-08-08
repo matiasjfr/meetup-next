0f71add5af1e65b3ec81d2eab5139a7713eef7fc
Matías Fitó <matias.fito.reussi@modo.com.ar>
2023-08-08 06:02:02 -0300

    fix: ci 7

--- src/components/custom/TableDemo/__test__/page.test.tsx
+++ src/components/custom/TableDemo/__test__/page.test.tsx
@@ -1,30 +1,10 @@
-import { EStatus } from '@prisma/client';
-import { render, RenderOptions } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import { TableDemo } from '..';
 
-const productsMock = [
-  {
-    id: 1,
-    name: 'Product 1',
-    description: '',
-    buyPrice: null,
-    sellPrice: null,
-    status: EStatus.ACTIVE,
-    image: 'https://loremflickr.com/100/100',
-    idCategory: 1,
-    category: {
-      id: 1,
-      name: 'Computers',
-      description: 'Outdoors',
-      status: EStatus.INACTIVE,
-    },
-  },
-];
-
 describe('TableDemo', () => {
   it('TableDemo correctly', () => {
-    const { container } = render(<TableDemo products={productsMock} />);
+    const { container } = render(<TableDemo products={[]} />);
     expect(container).toMatchSnapshot();
   });
 });
